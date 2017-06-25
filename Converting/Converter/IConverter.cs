eb152409679acc60d79516bbcc5147b0388a2a88
VladListing <chvch@mail.ru>
2017-06-25 23:17:43 +0700

    creation interface 'IConverter' in the Library 'Converter'

---
+++ Converting/Converter/IConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    ///<symmary>
+    ///интерфейс 'IConverter' устанавливает методы по конвертации и обромлению в задачу.
+    ///<symmary>
+
+    interface IConverter
+    {
+        //метод 'Convert' преобразует файл из одного формата в другой
+        void Convert(string srcPath, string destPath);
+
+        //Метод 'GetConvertAsync' возвращает фоновую задачу 
+        Task GetConvertAsync(string srcPath, string destPath);
+
+    }
+}
