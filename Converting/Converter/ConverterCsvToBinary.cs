7e050cb0cb69f32ad4182ed668378c2549fc3583
VladListing <chvch@mail.ru>
2017-06-25 23:45:38 +0700

    creation class 'ConverterCsvToBinary' in the Library 'Converter'

---
+++ Converting/Converter/ConverterCsvToBinary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    /// <summary>
+    /// класс 'ConverterCsvToBinary' преобразует файл из формата Csv в бинарный,
+    /// а так же обрамляет в фоновую задачу.
+    /// </summary>
+
+
+    class ConverterCsvToBinary
+    {
+        //метод 'Convert', преобразует файл из формата Csv в бинарный.
+        public void Convert(string srcPath, string destPath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        //метод 'GetConvertAsync'
+        public Task GetConvertAsync(string srcPath, string destPath)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
